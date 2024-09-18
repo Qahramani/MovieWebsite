6756145047a774db5f47b82bddb1a03badd1b0b3
fatoncikbatoncik <qahramanigunel@gmail.com>
2024-09-18 17:25:32 +0400

    first commit

---
+++ scripts/details.js
@@ -0,0 +1,45 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+const getDetailedData = async () => {
+try{
+  const id = urlParams.get("id");
+  console.log(id)
+
+  const response = await fetch(
+    `https://api.tvmaze.com/shows/${id}`
+  );
+
+  const json = await response.json();
+
+  const output=document.querySelector(".details-container")
+
+
+  output.innerHTML=
+  `
+  <div class="img-div">
+          <img src="${json.image.original}" alt="">
+        </div>
+        <div class="info-container">
+        <div class="details-div"> 
+                <h3 class="name-text">${json.name}</h3>
+                <p class="lang-text">Language : ${json.language}</p>
+                <p class="rating-text">Rating : ${json.rating.average}</p>
+                <p class="duration-text">Runtime: ${json.runtime}minutes</p>
+                <p class="sescrb-text"><b>Description :</b><br>  
+                 ${json.summary}</p>
+             </div> 
+         
+        </div>
+        `;
+
+  console.log(json);
+
+}catch(e){
+  // alert("Data tapilmadi")
+  window.location.href = "error.html";
+}
+
+};
+
+getDetailedData();
---
+++ scripts/main.js
@@ -0,0 +1,52 @@
+// const queryString = window.location.search;
+// const urlParams = new URLSearchParams(queryString);
+
+// const id = urlParams.get('id');
+// const name = urlParams.get('name');
+
+// console.log(id, name);
+ 
+const getData = async () => {
+  const response = await fetch("https://api.tvmaze.com/shows");
+
+  const json = await response.json();
+
+    console.log(json);
+
+  return json;
+};
+
+
+
+const renderList = (data, page) => {
+
+  for (let i = (page-1)*10; i < page * 10; i++) {
+    const element = data[i];
+
+    const list=document.querySelector(".highest-rating-scrole")
+
+    list.innerHTML += `<a href="details.html?id=${element.id}" class="movie-context">
+            
+              <img src="${element.image.original}" alt="" />
+               <div class="movie-context-pointed">
+              <h3>${element.name}</h3>
+               <p>Rating : ${element.rating.average}</p>
+              </div>
+           
+            </a>
+  `;
+}
+};
+
+const data = await getData();
+
+const loadMoreBtn=document.querySelector(".load-more-btn");
+
+let currentPage=1;
+ 
+loadMoreBtn.addEventListener("click",()=>{
+  currentPage++;
+  renderList(data,currentPage)
+})
+
+renderList(data, currentPage);
